3aea9ad774f8f2ad0571e57ff394901ee11c6faf
Mahammad <quseynovmaqa25@gmail.com>
2024-11-03 01:05:00 +0400

    I have completed the task to display even and odd numbers.

--- src/main/java/az/edu/turing/module01/lesson03/NumberApp.java
+++ src/main/java/az/edu/turing/module01/lesson03/NumberApp.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class NumberApp {
     public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Enter a number");
-//        int num = scan.nextInt();
-//        System.out.println((num == 0) ? "hec biri deyil" : (num % 2 == 1) ? "Tekdir" : "cutdur");
+       Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = scan.nextInt();
+        System.out.println((num == 0) ? "hec biri deyil" : (num % 2 == 1) ? "Tekdir" : "cutdur");
 
 
 
